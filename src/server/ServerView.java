6fd41c98d556a2a5e31b2cd6b9e4dc76f2091041
Burri-PC <Burri-PC@10.207.4.192>
2016-04-12 19:47:57 +0200

    added picture

--- src/server/ServerView.java
+++ src/server/ServerView.java
@@ -1,8 +1,12 @@
 package server;
 
+import java.io.File;
+
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane; 
 import javafx.stage.Stage;
 
 public class ServerView {
@@ -14,8 +18,13 @@ public class ServerView {
 		this.primaryStage = primaryStage;
 		this.model = model;
 		
+
+		
 		BorderPane topPane = new BorderPane();
+		Image image = new Image("/images/NichtLustig_Logo.jpg");
+		ImageView logo = new ImageView(image);
 		topPane.setCenter(new Label("Hello World"));
+		topPane.setTop(logo);
 		
 		Scene scene = new Scene(topPane);
 		primaryStage.setScene(scene);
