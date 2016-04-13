8e6094e01438a2e8f6b968bc8dd6da66f521cb97
kevin.trottmann <kevin.trottmann@students.fhnw.ch>
2016-04-13 17:40:07 +0200

    Client Kevin

--- src/client/ClientView.java
+++ src/client/ClientView.java
@@ -1,10 +1,13 @@
 package client;
 
+
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane; 
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class ClientView {
@@ -16,19 +19,45 @@ public class ClientView {
 		this.primaryStage = primaryStage;
 		this.model = model;
 		
-		//Pane 
+		//TopPane 
 		BorderPane topPane = new BorderPane();
-		Image image = new Image("/images/NichtLustig_Logo.jpg");
-		ImageView logo = new ImageView(image);
-		topPane.setTop(logo);
 		
-		//Username Label
+		//InnerPane
+		GridPane innerPane = new GridPane();
+		innerPane.setAlignment(Pos.TOP_CENTER);
+		innerPane.setHgap(10);
+		innerPane.setVgap(10);
+		topPane.setCenter(innerPane);
 		
+		//Label Username
+		Label lb_username = new Label("Username: ");
+		innerPane.add(lb_username, 0, 45);
+		lb_username.setId("lb-username");
 		
+		//Label Password
+		Label lb_password = new Label("Passwort: ");
+		innerPane.add(lb_password, 0, 48);
+		lb_password.setId("lb-password");
 		
+		//Textbox Username
+		TextField tf_username = new TextField();
+		innerPane.add(tf_username, 3, 45);
+		tf_username.setId("tf-username");
+		
+		//Textbox Password
+		TextField tf_password = new TextField();
+		innerPane.add(tf_password, 3, 48);
+		tf_password.setId("tf-password");
+		
+		//Button Login
+		Button b_login = new Button("Login");
+		innerPane.add(b_login, 3, 51);
+		b_login.setId("b-login");
 		
 		//Scene
-		Scene scene = new Scene(topPane, 600, 800);
+		Scene scene = new Scene(topPane, 1200, 800);
+		scene.getStylesheets().add("ClientStylesheet");
+		primaryStage.setTitle("Client-Applikation");
 		primaryStage.setScene(scene);
 		primaryStage.setResizable(false);
 	}
