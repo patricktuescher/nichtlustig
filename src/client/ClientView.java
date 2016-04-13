77fae14902526086afc5bb2c3b22689a62c13906
kevin.trottmann <kevin.trottmann@students.fhnw.ch>
2016-04-13 12:45:12 +0200

    Test

--- src/client/ClientView.java
+++ src/client/ClientView.java
@@ -1,8 +1,5 @@
 package client;
 
-
-import java.io.File;
-
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -19,16 +16,21 @@ public class ClientView {
 		this.primaryStage = primaryStage;
 		this.model = model;
 		
-
-		
+		//Pane 
 		BorderPane topPane = new BorderPane();
 		Image image = new Image("/images/NichtLustig_Logo.jpg");
 		ImageView logo = new ImageView(image);
-		topPane.setCenter(new Label("Hello World"));
 		topPane.setTop(logo);
 		
-		Scene scene = new Scene(topPane);
+		//Username Label
+		
+		
+		
+		
+		//Scene
+		Scene scene = new Scene(topPane, 600, 800);
 		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
 	}
 	
 	public void start(){
