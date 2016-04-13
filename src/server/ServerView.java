2f22a0678a1183b4c28eeff8b07b0a3df64a7163
scuib <scuib@DESKTOP-DSV696Q.home>
2016-04-13 13:31:31 +0200

    added buttons on server application and edited css file

--- src/server/ServerView.java
+++ src/server/ServerView.java
@@ -1,12 +1,15 @@
 package server;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ServerView {
@@ -19,21 +22,31 @@ public class ServerView {
 		this.primaryStage = primaryStage;
 		this.model = model;
 		
-		
+		//Setting up BorderPane
 		BorderPane topPane = new BorderPane();
-		topPane.setId("topPane");
+		topPane.setId("topPane"); //ID for CSS
 		topPane.setPrefHeight(800);
 		topPane.setPrefWidth(1200);
-		HBox h1 = new HBox();
-		h1.setId("h1");
-		topPane.setCenter(h1);
-		button1 = new Button("Hello World");
-		topPane.setBottom(button1);
+		
+		Button ConnectServer = new Button("Connect to Server");
+		ConnectServer.setId("ServerButtons");
+		ConnectServer.setPrefWidth(500);
+		Button DisconnectServer = new Button("Disconnect  to Server");
+		DisconnectServer.setId("ServerButtons");
+		DisconnectServer.setPrefWidth(500);
+
+		GridPane innerPane = new GridPane();
+		innerPane.setAlignment(Pos.TOP_CENTER);
+		innerPane.setHgap(10);
+		innerPane.setVgap(100);
+		innerPane.add(ConnectServer, 0, 5);
+		innerPane.add(DisconnectServer, 0, 6);
+		topPane.setCenter(innerPane);
 		
 		Scene scene = new Scene(topPane);
-		//topPane.setStyle("-fx-background-image: url(/images/Server_Background.jpg)");
 		scene.getStylesheets().add("stylesheet");
-		
+		primaryStage.setTitle("Server-Applikation");
+		primaryStage.setResizable(false);
 		primaryStage.setScene(scene);
 	}
 	
