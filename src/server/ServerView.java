05c487de32fb1a114bf68645183f9f3d4dc2a169
scuib <scuib@DESKTOP-DSV696Q.home>
2016-04-13 10:45:08 +0200

    set background image for server application

--- src/server/ServerView.java
+++ src/server/ServerView.java
@@ -1,16 +1,19 @@
 package server;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane; 
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class ServerView {
 	
 	Stage primaryStage;
 	ServerModel model;
+	Button button1;
 	
 	public ServerView(Stage primaryStage, ServerModel model){
 		this.primaryStage = primaryStage;
@@ -18,13 +21,19 @@ public class ServerView {
 		
 		
 		BorderPane topPane = new BorderPane();
-		topPane.setPrefHeight(800);
-		topPane.setPrefWidth(1200);		
 		topPane.setId("topPane");
+		topPane.setPrefHeight(800);
+		topPane.setPrefWidth(1200);
+		HBox h1 = new HBox();
+		h1.setId("h1");
+		topPane.setCenter(h1);
+		button1 = new Button("Hello World");
+		topPane.setBottom(button1);
 		
 		Scene scene = new Scene(topPane);
 		//topPane.setStyle("-fx-background-image: url(/images/Server_Background.jpg)");
-		scene.getStylesheets().add("style1.css");
+		scene.getStylesheets().add("stylesheet");
+		
 		primaryStage.setScene(scene);
 	}
 	
