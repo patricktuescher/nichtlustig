fcef629c1579eebee00be10946ef50e5a91705b1
Marco <takuma@bluewin.ch>
2016-05-21 13:33:50 +0200

    asda

---
+++ src/server/ServerKlasse.java
@@ -0,0 +1,55 @@
+package server;
+
+public class ServerKlasse {
+
+	
+	//class- name & location needs to be adjusted
+	
+	
+	///// attributs /////
+	
+	private String language;
+	private String connectedClients;
+	private int connectionStatus;
+	
+	
+	
+	
+	
+	///// methods /////
+	
+	public void sendData(){
+		
+	}
+	
+	public void receiveData(){
+		
+	}
+
+	public void checkConnection(){
+	
+	}
+
+	public void establishConnection(){
+		
+	}
+	
+	public void closeConnection(){
+		
+	}
+	
+	public void exitServer(){
+		
+	}
+		
+	
+	public void startServer(){
+		
+	}
+		
+	public void changeLanguage(){
+		
+	}
+	
+	
+}
