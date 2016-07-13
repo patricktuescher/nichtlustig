9fa57a4893a2d4b3a84cf91a5b55f19b20981781
Burri-PC <Burri-PC@10.207.1.235>
2016-07-13 07:24:01 +0200

    chatMessage

--- src/message/ChatMessage.java
+++ src/message/ChatMessage.java
@@ -1,5 +1,20 @@
 package message;
 
-public class ChatMessage {
+import java.io.Serializable;
+
+public class ChatMessage implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6059145399295545383L;
+	private String msg;
+	
+	public ChatMessage(String msg){
+		this.msg = msg;
+	}
+	
+	public String getMessage(){
+		return msg;
+	}
 
 }
