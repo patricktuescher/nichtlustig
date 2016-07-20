b90d294b36b174d49991bb062049e9dbda585969
Burri-PC <Burri-PC@DESKTOP-UJCEA7M>
2016-07-20 07:33:34 +0200

    minor

--- src/client/ClientController.java
+++ src/client/ClientController.java
@@ -410,8 +410,7 @@ public class ClientController {
 	public void würfeln(){
 		for(int x = 0; x < view.WürfelPL1.size(); x++){
 			if(!view.WürfelPL1.get(x).isSelected()){
-			view.WürfelPL1.get(x).roll();
-			view.WürfelPL1.set(x, view.WürfelPL1.get(x));
+
 			}
 			
 			}
