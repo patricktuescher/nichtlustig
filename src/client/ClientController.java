41e9ac0a26d67156c41c6130a036232b3b47087f
Patrick Tuescher <patrick.tuescher@students.fhnw.ch>
2016-08-08 13:13:44 +0200

    bugfix prof

--- src/client/ClientController.java
+++ src/client/ClientController.java
@@ -995,7 +995,7 @@ public class ClientController {
 	 public boolean profCard(){
 		 boolean b = false;
 		 for(int x = 0; x < view.cardAL.size(); x++){
-			 if(view.cardAL.get(x).getOwner() != null && view.cardAL.get(x).getType().equals("Prof") && view.cardAL.get(x).getOwner().equals(clientOwner)){
+			 if(view.cardAL.get(x).getOwner() != null && view.cardAL.get(x).getType().equals("Prof") && view.cardAL.get(x).getOwner().equals(clientOwner) && view.cardAL.get(x).getStatus().equals(Status.gewertet)){
 				 b = true;
 			 }
 		 }
